ad9498a050b86b69423044071624ee15d19a4fe1
FortyThieves40 <danayoung1.0@gmail.com>
2016-02-02 16:08:17 -0800

    Stanford Calc
    Nearly to the end of lecture 3, stopped due to issue with operations.

--- stanfordCalc/ViewController.swift
+++ stanfordCalc/ViewController.swift
@@ -8,18 +8,92 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
+    
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var display: UILabel!
+    
+    var isUserInMiddleOfTypingNewNumber = false;
+    
+    var brain = CalcBrain()
+
+    @IBAction func digit(sender: UIButton)
+    {
+        let digit = sender.currentTitle!
+        //print("digit = \(digit)")
+        
+        if isUserInMiddleOfTypingNewNumber
+        {
+            display.text = display.text! + digit
+            print("digit = \(digit)")        }
+        else
+        {
+            display.text = digit;
+            isUserInMiddleOfTypingNewNumber = true;
+        }
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    @IBAction func operate(sender: UIButton)
+    {
+        if let operation = sender.currentTitle
+        {
+            if let result = brain.preformOperation(operation){
+                displayValue = result
+            }
+            else {
+                displayValue = 0
+            }
+        }
+        if isUserInMiddleOfTypingNewNumber
+        {
+            enter()
+        }
     }
+    
+//    func performOperation (operation: (Double, Double) -> Double)
+//    {
+//        if operandStack.count >= 2
+//        {
+//            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+//            enter()
+//        }
+//    }
+//
+//    @objc (preformOperation2:) func performOperation (operation: Double ->Double){
+//        if operandStack.count >= 1{
+//            displayValue = operation(operandStack.removeLast())
+//            enter()
+//        }
+//    }
 
-
+    @IBAction func enter()
+    {
+        isUserInMiddleOfTypingNewNumber = false;
+        if let result = brain.pushOperand(displayValue){
+            displayValue = result
+        }
+        else{
+            displayValue = 0
+        }
+        
+        //operandStack.append(displayValue)
+        //print("operandStack = \(operandStack)")
+        
+    }
+    
+    var displayValue: Double
+    {
+        get{
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set{
+            display.text = "\(newValue)"
+            isUserInMiddleOfTypingNewNumber = false
+        }
+    }
+    
 }
 
